5d353914ea7679afa4fae0377dc3f44582483780
shreyabhatt025 <bhattshreya024@gmail.com>
2025-11-08 11:10:29 +0530

    Updated authController.js with new signup/login logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,8 @@
 const router = require('express').Router();  //mini express object banata hai jo sirf routes handle karta hai.
 const bcrypt = require('bcryptjs'); // password ko encrypt  karne ke liye
 const User = require('./../models/user'); // user model import kar rahe hai
+const jwt = require('jsonwebtoken'); // json web token ko import kar rahe hai jo authentication ke liye use hota hai line76 
+
 
 
 // SIGNUP ENDPOINT 
@@ -42,6 +44,60 @@ router.post('/signup', async (req, res) => {
       success: false
     });
   }
+}) 
+
+// LOGIN ENDPOINT API 
+router.post('/login', async (req, res) => {
+    // logicc for login  
+  try{
+       
+        // check if user exists 
+
+        const user = await User.findOne({email:req.body.email});
+        if(!user) // if user doesnt exist 
+        {
+            return res.send({
+                message: "User does not exist",
+                success: false         
+              }); 
+        }
+
+
+        // if user exist then check if password is correct 
+        const isvalid = await bcrypt.compare(req.body.password,user.password); 
+        if(!isvalid) // if password is incorrect
+        {
+            return res.send({  
+                message: "Incorrect Password",
+                success: false          // means user in not logged in
+              });
+        }
+
+        //if email exist and password is correct then we are going to authenticate user , we are going to create authentication token using jwt and send that token in response 
+
+        // npm i jsonwebtoken = ye package install karna hai
+        // to create jsonwebtoken we need 2 things DATA , SECRET KEY ( cretaed in config.env file L3)
+
+        const token = jwt.sing({
+          userId: user._id} , process.env.SECRET_KEY , {expiresIn: "1d"} ); 
+
+          res.send({
+            message: "User logged in successfully",
+            success: true,          // means user is logged in 
+            dtoken :  token             // ye token hum client ko bhej rahe hai jise wo apne local storage me rakh lega 
+          }); 
+      
+
+
+
+
+   }catch(error)
+   {
+    res.send({
+        message: error.message,
+        success: false          // means user in not logged in 
+      }); 
+   }
 });
 
 module.exports = router;
